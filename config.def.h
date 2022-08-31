ec0383379b26820294b5db1c7f18ca237864736b
Karl Fredin <karlfredin@gmail.com>
2022-08-31 16:37:10 +0300

    adding my own config

--- config.def.h
+++ config.def.h
@@ -7,6 +7,7 @@ static const char *colorname[NUMCOLS] = {
 	[INIT] =   "2d2d2d",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
+	[CAPS] = "red",         /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
